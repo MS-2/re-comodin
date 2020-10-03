74f34acacc701084cebfb12cae927afb71d94886
MS-2 <juanjosejjjj@gmail.com>
2020-10-03 18:07:15 -0300

    Updates

---
+++ precache-manifest.c2956bbf57778512a428001fd101d06f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f874b21df005dca0942da0e3919ed4",
+    "url": "/re-comodin/index.html"
+  },
+  {
+    "revision": "774502b287e74809acdc",
+    "url": "/re-comodin/static/css/2.5f5ed2d0.chunk.css"
+  },
+  {
+    "revision": "a389241f40ec278ce0e8",
+    "url": "/re-comodin/static/css/main.de089d38.chunk.css"
+  },
+  {
+    "revision": "774502b287e74809acdc",
+    "url": "/re-comodin/static/js/2.3ebcfa17.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/re-comodin/static/js/2.3ebcfa17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a389241f40ec278ce0e8",
+    "url": "/re-comodin/static/js/main.4b9ec141.chunk.js"
+  },
+  {
+    "revision": "700f8cfd0788af1d98cf",
+    "url": "/re-comodin/static/js/runtime-main.f0f01fee.js"
+  },
+  {
+    "revision": "84541d3885773beada52f8830127a3a2",
+    "url": "/re-comodin/static/media/LogoComodin.84541d38.png"
+  },
+  {
+    "revision": "6c59861b0722c1c7bf3bc9514be289f3",
+    "url": "/re-comodin/static/media/quattro.6c59861b.png"
+  }
+]);
